df56bc0cfc49500d933553c4c1a034cfbe896712
209410678 <209410678@gms.tku.edu.tw>
2021-10-14 21:48:25 +0800

    first commit

---
+++ src/App_78.js
@@ -0,0 +1,12 @@
+import './App_78.scss';
+import Homepage_78 from './pages/Homepage_78';
+
+function App_78() {
+  return (
+    <div>
+      <Homepage_78 />
+    </div >
+  );
+}
+
+export default App_78;
---
+++ src/pages/Homepage_78.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Directory_78 from '../components/Directory_78';
+import './Homepage_78.scss';
+
+const Homepage_78 = () => {
+    return (
+        <div className='homepage'>
+            <Directory_78 />
+        </div>
+    )
+}
+
+export default Homepage_78;
