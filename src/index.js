acd5913280bd3d0899a3179410c4fd6d37e0c9d9
209410678 <209410678@gms.tku.edu.tw>
2021-10-21 22:00:57 +0800

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom/BrowserRouter';
 import './index.css';
 import App_78 from './App_78';
 import reportWebVitals from './reportWebVitals';
